abf96d4dc2796fc53a67f310d8d5c9108b811542
zhangyaxin742 <zhangyaxin742@gmail.com>
2025-07-16 02:10:04 -0400

    Vercel v2

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+      <Analytics />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/curriculum" element={<Curriculum />} />
