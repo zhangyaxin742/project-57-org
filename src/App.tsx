44174bc14177ecee1ab705b93ee0b3f745b24b8b
zhangyaxin742 <zhangyaxin742@gmail.com>
2025-07-16 02:13:36 -0400

    diddy

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
