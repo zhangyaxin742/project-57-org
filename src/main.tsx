960bc5264f86fa4e4d19e2ccdd1d1860545e6b08
zhangyaxin742 <zhangyaxin742@gmail.com>
2025-10-03 18:30:41 -0400

    theme pt 2

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { HelmetProvider } from 'react-helmet-async'
+import { ThemeProvider } from './contexts/ThemeContext'
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </HelmetProvider>
 );
